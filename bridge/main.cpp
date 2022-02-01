ce63e3d9b81960e978d5b3636ad168bac3536604
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2022-02-01 11:58:18 +0800

    Initial Commit

---
+++ bridge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bridge
+//
+//  Created by 冀宸 on 2022/2/1.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
